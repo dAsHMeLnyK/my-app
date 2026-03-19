a53cb9a8a1def85e885df91bb0d09fe8351ea075
dAsHMeLnyK <daryna.melnyk@oa.edu.ua>
2026-03-19 17:28:56 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,10 @@ const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
-  }
+  },
+  basePath: '/my-app',
+  assetPrefix: '/my-app/',
+
 };
 
 export default nextConfig;
